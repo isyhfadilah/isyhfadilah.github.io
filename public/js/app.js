5d9288ecebe646ffebed64d9e6920833fcd1f479
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-03-19 01:00:44 +0700

    Mengubah model navbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector("#hamburger")
 const menu = document.querySelector("#menu")
+const tutup = document.querySelector("#tutup")
 
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
-    hamburger.classList.toggle("bg-white")
+    hamburger.classList.toggle("hidden")
+    tutup.classList.toggle("hidden")
+})
+
+tutup.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden")
+    hamburger.classList.toggle("hidden")
+    tutup.classList.toggle("hidden")
 })
